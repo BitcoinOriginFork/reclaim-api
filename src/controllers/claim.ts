eadf7194ff0134aee188512c2e808a16cb1f2f95
Sam Jeston <sam.jeston@gmail.com>
2018-04-12 21:34:17 +1000

    Correct claim paramets

--- src/controllers/claim.ts
+++ src/controllers/claim.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express'
 import { createClaim, getClaimByBtcoAddress } from '../db/claims'
-import { Chain, validateClaim } from '../crypto'
+import { Chain, validateClaim, validEthAddress } from '../crypto'
 
 export async function getClaim (req: Request, res: Response, next: Function) {
   try {
@@ -20,23 +20,30 @@ export async function getClaim (req: Request, res: Response, next: Function) {
 // exists against the btcoAddress. This should produce a human readable error
 export async function postClaim (req: Request, res: Response, next: Function) {
   try {
-    const btcoAddress = req.body.btcoAddress
     const signature = req.body.signature
     const chain: Chain = req.body.chain
-    const chainAddress = req.body.signature
-    const message = req.body.signature
+    const address = req.body.address
+    const message = req.body.message
 
-    if (!btcoAddress || !signature || !chain || !chainAddress || !message) {
+    if (!signature || !chain || !address || !message) {
       res.status(400).json({message: 'Missing field'})
       return
     }
 
-    const validClaim = validateClaim(btcoAddress, chain, chainAddress, message, signature)
-    if (!validClaim) {
-      res.status(400).json({message: 'Invalid claim'})
+    const btcoAddress = message.split(':')[1]
+    const validClaimToAddress = validEthAddress(btcoAddress)
+    if (!validClaimToAddress) {
+      res.status(400).json({message: 'Invalid message. Ensure the form is BTCO:YourClaimAddress'})
+      return
     }
 
-    const claim = await createClaim(btcoAddress, signature, chain, chainAddress, message)
+    const validClaim = validateClaim(btcoAddress, chain, address, message, signature)
+    if (!validClaim) {
+      res.status(400).json({message: 'Invalid claim'})
+      return
+    }
+
+    const claim = await createClaim(btcoAddress, signature, chain, address, message)
     res.status(201).json(claim)
   } catch (e) {
     console.error(e)
